01f1a2c2e1f9d899e87786ebaa5a49f6bde1e639
Emily Morgado <emorgado@sfomlp-1jchv2h.ad.businesswire.com>
2018-09-20 17:06:10 -0700

    Broke last testing project, started anew. First test is functioning

---
+++ src/components/__tests__/App.test.js
@@ -0,0 +1,23 @@
+// We could have called this file App.js
+// But that might be less clear
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from '../App';
+
+// it = App.test.js
+it('shows a comment box', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<App />, div);
+  // Here we can inpect the div that lives inside JSDOM
+  // Looks inside the div
+  // and checks tot see if the CommentBox is in there
+  console.log(div.innHTML);
+
+  // Clean-up: for performance
+  // Destroys leftover objects
+  // If we don't call this, the component will remain for as long as the
+  // test suite is running, which slows down the running of large
+  // test suites
+  ReactDOM.unmountComponentAtNode(div);
+});
