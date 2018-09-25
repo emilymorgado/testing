6a838852ff597f78842a028044c78c7bed780668
Emily Morgado <emorgado@sfomlp-1jchv2h.ad.businesswire.com>
2018-09-25 16:54:02 -0700

    Sign In/Out button toggles as changeAuth() is called

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,26 @@
 import React, { Component } from 'react';
 import { Route, Link } from 'react-router-dom';
+import { connect } from 'react-redux';
 
 import CommentBox from 'components/CommentBox';
 import CommentList from 'components/CommentList';
+import * as actions from 'actions';
 
-export default class App extends Component {
+class App extends Component {
   renderButton() {
-
+    if (this.props.auth) {
+      return (
+        <button onClick={() => this.props.changeAuth(false)}>
+          Sign Out
+        </button>
+      );
+    } else {
+      return (
+        <button onClick={() => this.props.changeAuth(true)}>
+          Sign In
+        </button>
+      );
+    }
   }
 
   renderHeader() {
@@ -26,9 +40,16 @@ export default class App extends Component {
   render() {
     return (
       <div>
+        {this.renderHeader()}
         <Route path='/post' component={CommentBox} />
         <Route path='/' exact component={CommentList} />
       </div>
     );
   }
 };
+
+function mapStateToProps(state) {
+  return { auth: state.auth };
+}
+
+export default connect(mapStateToProps, actions)(App);
