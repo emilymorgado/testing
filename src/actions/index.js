632b42d02009abcbf793bf427d30cbfe0481aefc
Emily Morgado <emorgado@sfomlp-1jchv2h.ad.businesswire.com>
2018-09-25 16:42:31 -0700

    Create navigation links and a sign in button

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-import { SAVE_COMMENT, FETCH_COMMENTS } from 'actions/types';
+import { SAVE_COMMENT, FETCH_COMMENTS, CHANGE_AUTH } from 'actions/types';
 
 export function saveComment(comment) {
   return {
@@ -16,4 +16,11 @@ export function fetchComments() {
     type: FETCH_COMMENTS,
     payload: response,
   };
+}
+
+export function changeAuth(isLoggedIn) {
+  return {
+    type: CHANGE_AUTH,
+    payload: isLoggedIn,
+  }
 }
